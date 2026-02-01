a8795b5c190d2abd64ed1855006c1072a801d4cc
ABPD2001 <a.puretemadi@gmail.com>
2026-02-01 21:55:08 +0330

    exception vector table is almost done.

--- boot.s
+++ boot.s
@@ -8,7 +8,7 @@ reset_handler:
 .ltorg
 
 .section vectors
-.equiv EL1_CUR_SYNC_HANDLER_TABLE,
+.equiv EL1_CUR_HANDLER_TABLE,
 .equiv EL1_CUR_IRQ_HANDLER_TABLE,
 .equiv EL1_CUR_FIQ_HANDLER_TABLE
 
@@ -21,6 +21,46 @@ reset_handler:
 .equiv AUX_BASE,#0x7E215000
 .equiv AUX_IRQ,#0x00
 .equiv AUX_MU_IIR_REG,#0x48
+
+.macro _exception_entry
+    stp x0,x1,[sp,#-16] 
+    stp x2,x3,[sp,#-16]        
+    stp x4,x5,[sp,#-16]    
+    stp x6,x7,[sp,#-16]    
+    stp x8,x9,[sp,#-16]    
+    stp x10,x11,[sp,#-16]    
+    stp x12,x13,[sp,#-16]    
+    stp x14,x15,[sp,#-16]    
+    stp x16,x17,[sp,#-16]    
+    stp x18,x19,[sp,#-16]    
+    stp x20,x21,[sp,#-16]    
+    stp x22,x23,[sp,#-16]    
+    stp x24,x25,[sp,#-16]    
+    stp x26,x27,[sp,#-16]    
+    stp x28,x29,[sp,#-16]    
+    
+    msr x0,SPSR_EL1 @ read pstate fields
+    msr x1,ELR_EL1 @ read exception link register
+    
+    ldp x30,x0,[sp,#-16]
+    ldp x1,x1,[sp,#-16] @ save ELR with padding
+.endm
+
+.macro _exception_irq_enable
+    msr x3,DAIF_EL1
+    and x3,0b1011 @ enable IRQs
+    mrs x3,DAIF_EL1 @ apply
+.endm
+
+.macro _exception_fiq_enable
+    msr x3,DAIF_EL1
+    and x3,0b0111 @ enable FIQs
+    mrs x3,DAIF_EL1 @ apply
+.endm
+
+.macro _serror_panic
+@handle
+.endm
 
 vector_table:
     .balign 128
@@ -35,30 +75,11 @@ vector_table:
     .balign 128
     eret
 
-    @ CURRENT EXECUTION LEVEL with SPx
+    @ <--- CURRENT EXECUTION LEVEL with SPx --->
     @ synchronous exception
     .balign 128
-    stp x0,x1,[sp,#-16] 
-    stp x2,x3,[sp,#-16]        
-    stp x4,x5,[sp,#-16]    
-    stp x6,x7,[sp,#-16]    
-    stp x8,x9,[sp,#-16]    
-    stp x10,x11,[sp,#-16]    
-    stp x12,x13,[sp,#-16]    
-    stp x14,x15,[sp,#-16]    
-    stp x16,x17,[sp,#-16]    
-    stp x18,x19,[sp,#-16]    
-    stp x20,x21,[sp,#-16]    
-    stp x22,x23,[sp,#-16]    
-    stp x24,x25,[sp,#-16]    
-    stp x26,x27,[sp,#-16]    
-    stp x28,x29,[sp,#-16]    
-    
-    msr x0,SPSR_EL1 @ read pstate fields
-    msr x1,ELR_EL1 @ read exception link register
-    
-    ldp x30,x0,[sp,#-16]
-    ldp x1,x1,[sp,#-16] @ save ELR with padding
+    _exception_entry
+    msr ESR_EL1,x0 @ read ESR_EL1
 
     mov x1,x0
     mov x2,x0
@@ -67,36 +88,18 @@ vector_table:
     lsr x1,#26 
     bfc x2,xzr,#25,#6 @ serperate instruction of sync
 
-    bl determine_id_sync
+    bl determine_id
 
-    mov x19,=EL1_CUR_SYNC_HANDLER_TABLE
+    ldr x19,=EL1_CUR_HANDLER_TABLE
     mul x19,#4 @ calculate callback relative address of table.
-    mov pc,x19 # call the callback (c handler)
 
-    b EL1_CUR_SYNC_RETURN
+    bl x19 # call the callback (c handler)
+
+    b EL1_CUR_RETURN
+    
     @ IRQ/vIRQ exception (reentrant)
     .balign 128
-    stp x0,x1,[sp,#-16] 
-    stp x2,x3,[sp,#-16]        
-    stp x4,x5,[sp,#-16]    
-    stp x6,x7,[sp,#-16]    
-    stp x8,x9,[sp,#-16]    
-    stp x10,x11,[sp,#-16]    
-    stp x12,x13,[sp,#-16]    
-    stp x14,x15,[sp,#-16]    
-    stp x16,x17,[sp,#-16]    
-    stp x18,x19,[sp,#-16]    
-    stp x20,x21,[sp,#-16]    
-    stp x22,x23,[sp,#-16]    
-    stp x24,x25,[sp,#-16]    
-    stp x26,x27,[sp,#-16]    
-    stp x28,x29,[sp,#-16]    
-    
-    msr x0,SPSR_EL1 @ read pstate fields
-    msr x1,ELR_EL1 @ read exception link register
-    
-    ldp x30,x0,[sp,#-16]
-    ldp x1,x1,[sp,#-16] @ save ELR with padding
+    _exception_entry
 
     @ read GIC-400 for IRQ id.
 
@@ -104,36 +107,15 @@ vector_table:
     mul x0,#4 @ calculate callback relative address of table.
     add x1,x1,x0
 
-    msr x3,DAIF_EL1
-    and x3,0b1011 @ enable IRQs
-    mrs x3,DAIF_EL1 @ apply
+    _exception_irq_enable
 
     bl x1 # call the callback (c handler)
 
-    b EL1_CUR_SYNC_RETURN
+    b EL1_CUR_RETURN
+    
     @ FIQ/vFIQ exception (reentrant)
     .balign 128
-    stp x0,x1,[sp,#-16] 
-    stp x2,x3,[sp,#-16]        
-    stp x4,x5,[sp,#-16]    
-    stp x6,x7,[sp,#-16]    
-    stp x8,x9,[sp,#-16]    
-    stp x10,x11,[sp,#-16]    
-    stp x12,x13,[sp,#-16]    
-    stp x14,x15,[sp,#-16]    
-    stp x16,x17,[sp,#-16]    
-    stp x18,x19,[sp,#-16]    
-    stp x20,x21,[sp,#-16]    
-    stp x22,x23,[sp,#-16]    
-    stp x24,x25,[sp,#-16]    
-    stp x26,x27,[sp,#-16]    
-    stp x28,x29,[sp,#-16]    
-    
-    msr x0,SPSR_EL1 @ read pstate fields
-    msr x1,ELR_EL1 @ read exception link register
-    
-    ldp x30,x0,[sp,#-16]
-    ldp x1,x1,[sp,#-16] @ save ELR with padding
+    _exception_entry
 
     @ read GIC-400 for FIQ id.
 
@@ -141,20 +123,85 @@ vector_table:
     mul x0,#4 @ calculate callback relative address of table.
     add x1,x1,x0
 
-    msr x3,DAIF_EL1
-    and x3,0b0111 @ enable IRQs
-    mrs x3,DAIF_EL1 @ apply
+    _exception_fiq_enable
 
     bl x1 # call the callback (c handler)
 
-    b EL1_CUR_SYNC_RETURN
+    b EL1_CUR_RETURN
+    
     @ SError/vSError exception (reentrant)
     .balign 128
+    _serror_panic
+
+    @ <--- LOWER EXECUTION LEVEL with SPx (Aarch64) --->
+    @ synchronous exception
+    .balign 128
+    _exception_entry
+    msr ESR_EL1,x0 @ read ESR_EL1
+
+    mov x1,x0
+    mov x2,x0
+    
+    bfc x1,xzr,#0,#26 @ seperate class of sync
+    lsr x1,#26 
+    bfc x2,xzr,#25,#6 @ serperate instruction of sync
+
+    bl determine_id
+
+    ldr x19,=EL1_LOWER_HANDLER_TABLE
+    mul x19,#4 @ calculate callback relative address of table.
+
+    bl x19 # call the callback (c handler)
+
+    b EL1_LOWER_RETURN
+    
+    @ IRQ/vIRQ exception
+    .balign 128
+    _exception_entry
+    @ read GIC-400 for IRQ id.
+
+    ldr x1,=EL1_LOWER_IRQ_HANDLER_TABLE
+    mul x0,#4 @ calculate callback relative address of table.
+    add x1,x1,x0
+    
+    _exception_irq_enable
+
+    bl x1 # call the callback (c handler)
+    b EL1_LOWER_RETURN
+
+    @ FIQ/vFIQ exception
+    .balign 128
+    _exception_entry
+    @ read GIC-400 for FIQ id.
+
+    ldr x1,=EL1_LOWER_FIQ_HANDLER_TABLE
+    mul x0,#4 @ calculate callback relative address of table.
+    add x1,x1,x0
+    
+    _exception_fiq_enable
+
+    bl x1 # call the callback (c handler)
+    bl EL1_LOWER_RETURN
+
+    @ SError/vSError exception
+    .balign
+    _serror_panic
+
+    @ <--- LOWER EXECUTION LEVEL with SPx (Aarch32) --->
+    .balign 128
+    eret
+    .balign 128
+    eret
+    .balign 128
+    eret
+    .balign 128
+    eret
+
 
 .ltorg
 
 .section vector_table_handlers
-EL1_CUR_SYNC_RETURN:
+EL1_CUR_RETURN:
     ldp x0,x0,[sp,#16] @ read ELR_EL1
     mrs x0,ELR_EL1 @ apply ELR_EL1.
     ldp x30,x0,[sp],#16 @ read x30 and SPSR_EL1    
@@ -177,7 +224,33 @@ EL1_CUR_SYNC_RETURN:
 
     eret @ return.
 
-determine_id_sync:
+EL1_LOWER_RETURN:
+    ldp x0,x0,[sp,#16] @ read ELR_EL1
+    mrs x0,ELR_EL1 @ apply ELR_EL1.
+    ldp x30,x0,[sp],#16 @ read x30 and SPSR_EL1    
+    mrs x0,SPSR_EL1 @ apply spsr.
+    ldp x28,x29,[sp],#16    
+    ldp x26,x27,[sp],#16    
+    ldp x24,x25,[sp],#16    
+    ldp x22,x23,[sp],#16    
+    ldp x20,x21,[sp],#16    
+    ldp x18,x19,[sp],#16    
+    ldp x16,x17,[sp],#16    
+    ldp x14,x15,[sp],#16    
+    ldp x12,x13,[sp],#16    
+    ldp x10,x11,[sp],#16    
+    ldp x8,x9,[sp],#16    
+    ldp x6,x7,[sp],#16    
+    ldp x4,x5,[sp],#16    
+    ldp x2,x3,[sp],#16        
+    ldp x0,x1,[sp],#16 @ restore context.
+
+    @ save current task context
+
+    bl task_schaduler
+    bl task_dispatcher
+
+determine_id:
     stp x29,x30,[sp,#-16]! @ store FP and LR.
     mov x29,sp @ set frame pointer.
 
