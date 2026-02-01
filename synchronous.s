191d8119d13799f03af7c36533250010e8fe4dee
ABPD2001 <a.puretemadi@gmail.com>
2026-02-01 23:10:18 +0330

    trace

--- synchronous.s
+++ synchronous.s
@@ -1,20 +1,20 @@
-.section .sync_table
-sync_table:
+.section .sync_same_el_table
+sync_same_el_table:
     .balign 4
-    .org 0x00, b unkown_handler 
-    .org 0x04, b wfi_wfe_handler 
-    .org 0x08, b simd_fp_err_handler 
-    .org 0x0C, b illegal_state_handler
-    .org 0x10, b svc_hanler
-    .org 0x14, b hvc_hanler
-    .org 0x18, b smc_hanler
-    .org 0x20, b i_abort_lower_hanler
-    .org 0x24, b i_abort_same_hanler
-    .org 0x28, b pc_alignment_hanler
-    .org 0x2C, b d_abort_lower_handler
-    .org 0x30, b d_abort_same_handler
-    .org 0x34, b sp_alignment_hanler
-    .org 0x38, b fp_error_handler
-    .org 0x40, b breakpoint_handler
-    .org 0x44, b step_handler
+    .org 0x00, b same_el_unkown_handler 
+    .org 0x04, b same_el_wfi_wfe_handler 
+    .org 0x08, b same_el_simd_fp_err_handler 
+    .org 0x0C, b same_el_illegal_state_handler
+    .org 0x10, b same_el_svc_hanler
+    .org 0x14, b same_el_hvc_hanler
+    .org 0x18, b same_el_smc_hanler
+    .org 0x20, b same_el_i_abort_lower_hanler
+    .org 0x24, b same_el_i_abort_same_hanler
+    .org 0x28, b same_el_pc_alignment_hanler
+    .org 0x2C, b same_el_d_abort_lower_handler
+    .org 0x30, b same_el_d_abort_same_handler
+    .org 0x34, b same_el_sp_alignment_hanler
+    .org 0x38, b same_el_fp_error_handler
+    .org 0x40, b same_el_breakpoint_handler
+    .org 0x44, b same_el_step_handler
 .ltorg
