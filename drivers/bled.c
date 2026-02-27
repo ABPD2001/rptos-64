6f292c47d48cc86f3bd05a63bc977bb231232b46
ABPD2001 <a.puretemadi@gmail.com>
2026-02-27 20:55:43 +0330

    trying to fix macro problem in bled.c file...

--- drivers/bled.c
+++ drivers/bled.c
@@ -6,37 +6,37 @@ void initialize_bleds()
     gpfunction(GPIO_FSEL4, 2, GPIO_OUTPUT); // set GPIO42 to output.
     gpfunction(GPIO_FSEL4, 5, GPIO_OUTPUT); // set GPIO45 to output.
 
-    gpclear(GPIO_CLR4, 10); // clear GPIO42 (just in case).
-    gpclear(GPIO_CLR4, 13); // clear GPIO45 (just in case).
+    gpclear(GPIO_OUT_CLR1, 10); // clear GPIO42 (just in case).
+    gpclear(GPIO_OUT_CLR1, 13); // clear GPIO45 (just in case).
 }
 
 void brled_on()
 {
-    gpset(GPIO_SET4, 13); // set GPIO45.
+    gpset(GPIO_OUT_SET1, 13); // set GPIO45.
 }
 void bgled_on()
 {
-    gpset(GPIO_SET4, 10); // set GPIO42.
+    gpset(GPIO_OUT_SET1, 10); // set GPIO42.
 }
 void brled_off()
 {
-    gpclear(GPIO_CLR4, 13); // clear GPIO45 (just in case).
+    gpclear(GPIO_OUT_CLR1, 13); // clear GPIO45 (just in case).
 }
 void bgled_off()
 {
-    gpclear(GPIO_CLR4, 10); // clear GPIO42 (just in case).
+    gpclear(GPIO_OUT_CLR1, 10); // clear GPIO42 (just in case).
 }
 void brled_set(u8_t value)
 {
     if (value)
-        gpclear(GPIO_CLR4, 13);
+        gpclear(GPIO_OUT_CLR1, 13);
     else
-        gpset(GPIO_SET4, 13);
+        gpset(GPIO_OUT_SET1, 13);
 }
 void bgled_set(u8_t value)
 {
     if (value)
-        gpset(GPIO_SET4, 10);
+        gpset(GPIO_OUT_SET1, 10);
     else
-        gpclear(GPIO_CLR4, 10);
+        gpclear(GPIO_OUT_CLR1, 10);
 }
