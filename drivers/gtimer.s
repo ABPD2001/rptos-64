cc172f858e8b00a985b55b028a9a000ce3f040d1
ABPD2001 <a.puretemadi@gmail.com>
2026-02-27 21:26:54 +0330

    trace

--- drivers/gtimer.s
+++ drivers/gtimer.s
@@ -7,9 +7,9 @@ turn_on_gtimer:
     stp x29,x30,[sp,#-16]!
     mov x29,sp
     
-    mrs x1,CNTP_CTL_EL1 @ read generic-timer control register.
-    orr x1,x1,0x1 @ set first bit (enable). 
-    msr CNTP_CTL_EL1,x1 @ apply chagnes.
+    mrs x1,CNTP_CTL_EL1     @ read generic-timer control register.
+    orr x1,x1,0x1           @ set first bit (enable). 
+    msr CNTP_CTL_EL1,x1     @ apply chagnes.
     
     ldp x29,x30,[sp],#16
     mov sp,x29
@@ -19,11 +19,11 @@ set_gtimer:
     stp x29,x30,[sp,#-16]!
     mov x29,sp
 
-    mrs x1,CNTFRQ_EL1 @ read generic-timer frequency.
+    mrs x1,CNTFRQ_EL1       @ read generic-timer frequency.
     mov x2,#1000
-    udiv x1,x1,x2 @ frequency/1000 
-    mul x1,x1,x0 @ (frequency/1000)*ms = genereic-timer counts.
-    msr x1,CNTP_TVAL_EL1 @ set generic-timer counts.
+    udiv x1,x1,x2           @ frequency/1000 
+    mul x1,x1,x0            @ (frequency/1000)*ms = genereic-timer counts.
+    msr x1,CNTP_TVAL_EL1    @ set generic-timer counts.
     
     ldp x29,x30,[sp],#16
     mov sp,x29
@@ -35,15 +35,15 @@ read_guptime:
     mrs x0,CNTPCT_EL1 @ read uptime counts.
     mrs x1,CNTFRQ_EL1 @ read generic-timer frequency.
     mov x2,#1000
-    mul x0,x0,x2 @ counts*1000 -> counts (seconds)
-    udiv x0,x0,x1 @ counts/frequency = milli seconds.
+    mul x0,x0,x2      @ counts*1000 -> counts (seconds)
+    udiv x0,x0,x1     @ counts/frequency = milli seconds.
 
     ldp x29,x30,[sp],#16
     ret
 
 read_gticks:
     stp x29,x30,[sp,#-16]!
-    mrs x0,CNTPCT_EL1 @ read ticks counts.
+    mrs x0,CNTPCT_EL1   @ read ticks counts.
     ldp x29,x30,[sp],#16
     ret
 
