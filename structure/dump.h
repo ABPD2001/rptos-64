5dc9ee30f2449fbd9ff5882be63f6e3d1385fc47
ABPD2001 <a.puretemadi@gmail.com>
2026-03-03 22:24:33 +0330

    some comments added.

--- structure/dump.h
+++ structure/dump.h
@@ -4,13 +4,13 @@
 
 struct task_dump_t // 56 Bytes.
 {
-    u64_t task_id;
-    u64_t preipherals;
-    u64_t preipherals_count;
-    u64_t fault_code;
-    u64_t fault_dump;
-    u64_t flags;
-    u64_t stime;
+    u64_t task_id;           // id of dumped task.
+    u64_t preipherals;       // allocated preipherals by task.
+    u64_t preipherals_count; // count of allocated preipherals by task.
+    u64_t fault_code;        // fault code.
+    u64_t fault_dump;        // fault dump (description).
+    u64_t flags;             // flags of task.
+    u64_t stime;             // value of timer (time of dump).
 };
 
 #endif
--- structure/exceptions.h
+++ structure/exceptions.h
@@ -4,10 +4,10 @@
 
 struct system_exceptions_statistics_t
 {
-    u64_t softwaresteps;
-    u64_t breakpoints;
-    u64_t kernel_wfi_wfe_counts;
-    u64_t kernel_stack_alignment_faults;
+    u64_t softwaresteps;                 // count of software step exceptions.
+    u64_t breakpoints;                   // count of breakpoint exceptions.
+    u64_t kernel_wfi_wfe_counts;         // count of wfi/wfe exceptions from EL1.
+    u64_t kernel_stack_alignment_faults; // count of stack alignment fault exceptions from EL1.
 };
 
 #endif
